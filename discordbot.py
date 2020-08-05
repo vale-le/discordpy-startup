082dad3c105987b1696cd3f37273f51c48e4dc45
vale-le <69229090+vale-le@users.noreply.github.com>
2020-08-05 21:22:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,15 +7,15 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 def make_team(ctx, adj):
-    include = set()
+    include = s[
     exclude = set()
     for mem in adj:
         if re.match(r'\+.+', mem):
-            include.append(mem[1:])
+            include.add(mem[1:])
         elif re.match(r'\-.+', mem):
-            exclude.append(mem[1:])
+            exclude.add(mem[1:])
     vc = ctx.author.voice
-    members = list({member.name for member in vc.channel.members} | include - exclude)
+    members = list(({member.name for member in vc.channel.members} | include) - exclude)
     return members
     random.shuffle(members)
 
