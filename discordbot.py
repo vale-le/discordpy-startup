7f2ae09e645cbd3d9d26eaca567bb2bf62129ea5
vale-le <69229090+vale-le@users.noreply.github.com>
2020-08-20 21:00:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -44,11 +44,11 @@ def make_team(ctx, adj):
     return msg
 
 
-# @bot.event
-# async def on_command_error(ctx, error):
-#     orig_error = getattr(error, "original", error)
-#     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-#     await ctx.send(error_msg)
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 
 @bot.command()
